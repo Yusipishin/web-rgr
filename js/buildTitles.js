0e0dfdb932c49998f92447a4367ef847b9d70610
Alexey <alexey.yus@mail.ru>
2024-04-02 19:55:41 +0300

    fix bug with titles

--- js/buildTitles.js
+++ js/buildTitles.js
@@ -2,7 +2,7 @@ const buildTitles = (data) => {
     data.map((title) => {
         selectTitles.forEach(item => {
             item.insertAdjacentHTML('beforeend', `
-                <option value=${title}>${title}</option>
+                <option value="${title}">${title}</option>
             `)
         })
     })
