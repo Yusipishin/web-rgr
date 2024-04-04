117eb0dc64c7dbb95679761345cb71dea747781b
Alexey <alexey.yus@mail.ru>
2024-04-04 17:18:24 +0300

    fix bug with allowance

--- js/updateOrder.js
+++ js/updateOrder.js
@@ -1,3 +1,5 @@
+let delayDays, firstDate, secondDate;
+
 const updateOrder = (event) => {
     event.preventDefault()
 
@@ -14,11 +16,22 @@ const updateOrder = (event) => {
 
     const date1 = orderData.issued;
     const date2 = orderData.returned;
+    daysBetweenDates(date1, date2)
 
-    let delayDays = daysBetweenDates(date1, date2) - orderData.days
-    delayDays = delayDays > 0 ? delayDays : 0
-    orderData.allowance = parseInt(delayDays * 3/2 * orderData.rate)
-    orderData.total = orderData.days * orderData.rate + orderData.allowance
+    if (firstDate.getTime() < secondDate.getTime() || firstDate.getTime() === secondDate.getTime()) {
+        if (daysBetweenDates(date1, date2) < orderData.days) {
+            delayDays = daysBetweenDates(date1, date2)
+            orderData.allowance = 0
+            orderData.total = delayDays * orderData.rate
+        } else {
+            delayDays = daysBetweenDates(date1, date2) - orderData.days
+            orderData.allowance = parseInt(delayDays * 3/2 * orderData.rate)
+            orderData.total = orderData.days * orderData.rate + orderData.allowance
+        }
+    } else {
+        orderData.allowance = 0
+        orderData.total = 0
+    }
 
     updateData('http://localhost:3000/orders', updateId, orderData)
         .then(() => getOrders())
@@ -28,8 +41,8 @@ const updateOrder = (event) => {
 
 function daysBetweenDates(date1, date2) {
     const oneDay = 24 * 60 * 60 * 1000;
-    const firstDate = new Date(date1);
-    const secondDate = new Date(date2);
+    firstDate = new Date(date1);
+    secondDate = new Date(date2);
 
     const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
     return diffDays;
